4141160decbe7615ccf5fcc34bcabafc375c2587
DoUWant2DevApp <93307121+DoUWant2DevApp@users.noreply.github.com>
2024-03-09 03:53:41 -0500

    update exports

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ import {
 } from "./files/isValidOdds"
 
 import { whatOddsFormats } from "./files/whatOddsFormats"
+import { oddsFormatsInfo, oddsTypesNames } from "./files/oddFormatInfo"
+import type { OddsTypeNameType } from "./files/oddFormatInfo"
 
 import {
   //decimal to
@@ -135,3 +137,73 @@ export const oddsAid = {
   },
   convertOddsFromXToY,
 }
+
+export {
+  // validation
+  isValidOdds,
+  isValidDecimalOdds,
+  isValidHongKongOdds,
+  isValidMalaysianOdds,
+  isValidIndonesianOdds,
+  isValidAmericanMoneylineOdds,
+  isValidFractionalOdds,
+  isValidProbability,
+  //what potential odds formats
+  whatOddsFormats,
+  //conversion
+  //decimal to
+  decimalToAmericanOdds,
+  decimalToHongKongOdds,
+  decimalToMalaysianOdds,
+  decimalToIndonesianOdds,
+  decimalToFractionalOdds,
+  decimalToProbability,
+  //american to
+  americanToDecimalOdds,
+  americanToHongKongOdds,
+  americanToMalaysianOdds,
+  americanToIndonesianOdds,
+  americanToFractionalOdds,
+  americanToProbability,
+  //hong kong to
+  hongKongToDecimalOdds,
+  hongKongToAmericanOdds,
+  hongKongToMalaysianOdds,
+  hongKongToIndonesianOdds,
+  hongKongToFractionalOdds,
+  hongKongToProbability,
+  //malaysian to
+  malaysianToDecimalOdds,
+  malaysianToAmericanOdds,
+  malaysianToHongKongOdds,
+  malaysianToIndonesianOdds,
+  malaysianToFractionalOdds,
+  malaysianToProbability,
+  //indonesian to
+  indonesianToDecimalOdds,
+  indonesianToAmericanOdds,
+  indonesianToHongKongOdds,
+  indonesianToMalaysianOdds,
+  indonesianToFractionalOdds,
+  indonesianToProbability,
+  //fractional to
+  fractionalToDecimalOdds,
+  fractionalToAmericanOdds,
+  fractionalToHongKongOdds,
+  fractionalToMalaysianOdds,
+  fractionalToIndonesianOdds,
+  fractionalToProbability,
+  //probability to
+  probabilityToDecimalOdds,
+  probabilityToAmericanOdds,
+  probabilityToHongKongOdds,
+  probabilityToMalaysianOdds,
+  probabilityToIndonesianOdds,
+  probabilityToFractionalOdds,
+  //convertFromXtoY
+  convertOddsFromXToY,
+  //info
+  oddsFormatsInfo,
+  oddsTypesNames,
+}
+export type { OddsTypeNameType }
